6f8fc337f2edaf5dc61865182b6b1c5cfaf3d0ae
Hunter Crosland <hunter@croslands.com>
2020-11-19 11:09:05 -0800

    Finished simple set

--- SimpleSet/SimpleSet.h
+++ SimpleSet/SimpleSet.h
@@ -3,121 +3,191 @@
 
 #include <vector>
 
+namespace pic10c {
 
-template<typename T>
-class simple_set {
-private:
-	std::vector<T> data;
-public:
-	simple_set() : data(std::vector<T>()) {}
-	~simple_set() {
-		std::vector<T>().swap(data);
-	}
-	void insert(const T& new_data) {
-		bool found = false;
-		for (const T check : data) {
-			if(check == new_data) {
-				found = true;
+	template<typename T>
+	class simple_set {
+
+	private:
+
+		std::vector<T> data;
+
+	public:
+		//default constructor which defaults data to empty vector
+		simple_set() : data(std::vector<T>()) {}
+		//destructor makes a swap with a newly-inititialized empty vector
+		~simple_set() {
+			std::vector<T>().swap(data);
+		}
+
+		/**
+		Conditionally inserts new_data into class data storage vector if data does not exist in vector a priori
+		@param new_data a const ref to a T value for insertion into class data storage vector
+		*/
+		void insert(const T& new_data) {
+			bool found = false;
+			// check if data exists in storage vector already and flag found if so
+			for (const T& check : data) {
+				if (check == new_data) {
+					found = true;
+				}
+			}
+			// if data not found, add to storage vector
+			if (!found) { data.push_back(new_data); }
+		}
+
+		/**
+		Loops over storage vector to erase a value specified in args, if value exists in storage vector
+		@param delete_data a const ref to a T value for deletion from class data storage vector
+		*/
+		void erase(const T& delete_data) {
+			for (auto it = data.begin(); it != data.end(); ++it) {
+				if (*it == delete_data) { data.erase(it); }
 			}
 		}
-		if(!found) { data.push_back(new_data); }
-	}
-	void erase(const T& delete_data) {
-		for (std::vector<T>::iterator it; it != data.end(); ++it) {
-			if (data[it] == delete_data) { data.erase(it); }
+
+		/**
+		Loops over storage vector to find a value specified in args, returns true if found
+		@param find_data a const ref to a T value for discovery in class data storage vector
+		*/
+		bool find(const T& find_data) {
+			bool found = false;
+			//check find_data against all data values in data 
+			for (const T& check : data) {
+				if (check == find_data) {
+					found = true;
+				}
+			}
+			return found;
 		}
-	}
-	bool find(const T& find_data) {
-		bool found = false;
-		for (const T check : data) {
-			if (check == find_data) {
-				found = true;
+
+		/**
+		Returns size of storage vector
+		*/
+		size_t size() {
+			return data.size();
+		}
+	};
+
+	template<typename T>
+	class simple_set<T*> {
+	private:
+		std::vector<T*> data;
+	public:
+		//default constructor which defaults data to empty vector
+		simple_set() : data(std::vector<T*>()) {}
+		//destructor makes a swap with a newly-initialized empty vector
+		~simple_set() {
+			std::vector<T*>().swap(data);
+		}
+
+		/**
+		Conditionally inserts new_data into class data storage vector if data does not exist in vector a priori
+		@param new_data a const pointer to a T val for insertion into class data storage vector
+		*/
+		void insert(T *const new_data) {
+			bool found = false;
+			for (const T* check : data) {
+				if (*check == *new_data) {
+					found = true;
+				}
+			}
+			if (!found) { data.push_back(new_data); }
+		}
+
+		/**
+		Loops over storage vector to erase a value specified in args, if value exists in storage vector
+		@param delete_data a const ref to a T value for deletion from class data storage vector
+		*/
+		void erase(const T& delete_data) {
+			for (auto it = data.begin(); it != data.end(); ++it) {
+				if (*(*it) == delete_data) { data.erase(it); break; }
 			}
 		}
-		if (found) { return found; }
-	}
-	size_t size() {
-		return data.size();
-	}
-};
 
-template<typename T>
-class simple_set<T*> {
-private:
-	std::vector<T*> data;
-public:
-	simple_set() : data(std::vector<T*>()) {}
-	~simple_set() {
-		std::vector<T*>().swap(data);
-	}
-	void insert(const T* new_data) {
-		bool found = false;
-		for (const T* check : data) {
-			if (*check == *new_data) {
-				found = true;
+		/**
+		Loops over storage vector to find a value specified in args, returns true if found
+		@param find_data a const ref to a T value for discovery in class data storage vector
+		*/
+		bool find(const T& find_data) {
+			bool found = false;
+			for (const T* check : data) {
+				if (*check == find_data) {
+					found = true;
+				}
 			}
+			return found;
 		}
-		if (!found) { data.push_back(new_data); }
-	}
-	void erase(const T& delete_data) {
-		for (std::vector<T*>::iterator it; it != data.end(); ++it) {
-			if (*(data[it]) == delete_data) { data.erase(it); }
-		}
-		
-	}
 
-	bool find(const T& find_data) {
-		bool found = false;
-		for (const T* check : data) {
-			if (*check == find_data) {
-				found = true;
-			}
+		/**
+		Returns size of storage vector
+		*/
+		size_t size() {
+			return data.size();
 		}
-		return found;
-	}
-	size_t size() {
-		return data.size();
-	}
-};
+	};
 
-template<>
-class simple_set<bool> {
-private:
-	std::vector<bool> data;
-public:
-	simple_set() : data(std::vector<bool>()) {}
-	~simple_set() {
-		std::vector<bool>().swap(data);
-	}
-	void insert(bool new_data) {
-		bool found = false;
-		for (size_t i = 0; i < data.size(); ++i) {
-			if (new_data == data[i]) {
-				data.erase(i);
+	template<>
+	class simple_set<bool> {
+	private:
+
+		std::vector<bool> data;
+
+	public:
+		//default constructor which defaults data to empty vector
+		simple_set() : data(std::vector<bool>()) {}
+		//destructor makes a swap with a newly-initialized empty vector
+		~simple_set() {
+			std::vector<bool>().swap(data);
+		}
+
+		/**
+		Conditionally inserts new_data into class data storage vector if data does not exist in vector a priori
+			If data does exist, deletes existing data
+		@param new_data a const ref to a bool value for insertion into class data storage vector
+		*/
+		void insert(const bool& new_data) {
+			for (auto it = data.begin(); it != data.end(); ++it) {
+				if (*it == new_data) { data.erase(it); }
 				return;
 			}
-		}
-	}
-
-	void erase(const bool delete_data) {
-		for (std::vector<bool>::iterator it; it != data.end(); ++it) {
-			if (data[it]) == delete_data) { data.erase(it); }
+			data.push_back(new_data);
 		}
 
-	}
-	bool find(const bool find_data) {
-		bool found = false;
-		for (const bool check : data) {
-			if (check == find_data) {
-				found = true;
+		/**
+		Loops over storage vector to erase a value specified in args, if value exists in storage vector
+		@param delete_data a const ref to a bool value for deletion from class data storage vector
+		*/
+		void erase(const bool& delete_data) {
+			for (auto it = data.begin(); it != data.end(); ++it) {
+				if (*it == delete_data) { data.erase(it); }
 			}
+
 		}
-		if (found) { return found; }
-	}
-	size_t size() {
-		return data.size();
-	}
-};
+
+		/**
+		Loops over storage vector to find a value specified in args, returns true if found
+		@param find_data a const ref to a bool value for discovery in class data storage vector
+		*/
+		bool find(const bool& find_data) {
+			bool found = false;
+			for (const bool check : data) {
+				if (check == find_data) {
+					found = true;
+				}
+			}
+			return found;
+		}
+
+		/**
+		Returns size of storage vector
+		*/
+		size_t size() {
+			return data.size();
+		}
+	};
+
+}
 
 
 #endif
